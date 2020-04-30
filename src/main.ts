37c4be35940cde26106faa325a7285c71f28fa50
Philipp Schemel <philippschemel@live.com>
2020-04-30 16:52:32 +0200

    Make API port configurable in .env file

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = 3000;
+  const port = process.env.API_PORT;
 
   app.enableCors();
 
